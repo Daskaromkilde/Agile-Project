5d3cab8df84a1a00d9b92fe325253f27a8b9382a
Jonathan <gusalmjow@student.gu.se>
2024-09-23 16:29:12 +0200

    fix to include timer in notif

--- lib/quest_info_screen.dart
+++ lib/quest_info_screen.dart
@@ -94,13 +94,17 @@ class _QuestInfoScreenState extends State<QuestInfoScreen> {
         }
 
         final now = DateTime.now();
-        if (now.hour == 14 || now.hour == 20 && !allTasksDone()){
+        if ((now.hour == 14 || now.hour == 20) && !allTasksDone()){
+          final hours = remainingTime.inHours;
+          final minutes = remainingTime.inMinutes.remainder(60);
           showDialog(
             context: context,
             builder: (BuildContext context) {
-              return const AlertDialog(
+              return AlertDialog(
                 title: Text('Daily Quest Incomplete!'),
-                content: Text('You haven\'t forgotten about your daily tasks right?'),
+                content: Text(
+                  'You haven\'t forgotten about your daily tasks right?\n\n'
+                  'Time is running out!, you have $hours hours and $minutes minutes left!',),
               );
             },
           );
